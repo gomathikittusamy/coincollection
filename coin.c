4a43c22c9f68deebbb641b65af774afdbab5697a
gomathikittusamy <gomsanu4@gmail.com>
2016-07-06 12:03:13 +0530

    Create coin.c

---
+++ coin.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,a[100],i,sum,b,coin=0;
+clrscr();
+printf("NO of coins");
+scanf("%d",&n);
+printf("Value of coins ");
+for(i=1;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("enter the value of sum");
+scanf("%d",&sum);
+for(i=0;sum!=0&&i<n;i++)
+{
+b=0;
+b=sum/a[n];
+if(b!=0)
+printf("%d coin of %d rs\n",b,a[n]);
+coin=coin+b;
+sum=sum%a[n];
+n--;
+}
+printf("Total coins is %d",coin);
+getch();
+}
